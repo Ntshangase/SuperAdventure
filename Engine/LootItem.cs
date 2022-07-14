9bfbc5a24db1fdf719f28e2984bada5b0b36b81a
Ntshangase <simphiwe323i@gmail.com>
2022-07-14 02:18:52 +0200

    fiing bugs in code

--- Engine/LootItem.cs
+++ Engine/LootItem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace Engine
 {
     public class LootItem
@@ -10,6 +11,7 @@ namespace Engine
         public Item Details { get; set; }
         public int DropPercentage { get; set; }
         public bool IsDefaultItem { get; set; }
+
         public LootItem(Item details, int dropPercentage, bool isDefaultItem)
         {
             Details = details;
--- Engine/Quest.cs
+++ Engine/Quest.cs
@@ -6,30 +6,21 @@ using System.Threading.Tasks;
 
 namespace Engine
 {
-    public class Quest
+    public class Player : LivingCreature
     {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public int RewardExperiencePoints { get; set; }
-        public int RewardGold { get; set; }
-<<<<<<< HEAD
-        public Item RewardItem { get; set; }
-        public List<QuestCompletionItem> QuestCompletionItems { get; set; }
-=======
->>>>>>> 53e8f127dde3ae4807a00c4a6e8d08ec7b674106
+        public int Gold { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+        public List<InventoryItem> Inventory { get; set; }
+        public List<PlayerQuest> Quests { get; set; }
 
-        public Quest (int id, string name, string description, int rewardExperiencePoints, int rewardGold)
+        public Player(int currentHitPoints, int maximumHitPoints, int gold, int experiencePoints, int level) : base(currentHitPoints, maximumHitPoints)
         {
-            ID = id;
-            Name = name;
-            Description = description;
-            RewardExperiencePoints = rewardExperiencePoints;
-            RewardGold = rewardGold;
-<<<<<<< HEAD
-            QuestCompletionItems = new List<QuestCompletionItem>();
-=======
->>>>>>> 53e8f127dde3ae4807a00c4a6e8d08ec7b674106
+            Gold = gold;
+            ExperiencePoints = experiencePoints;
+            Level = level;
+            Inventory = new List<InventoryItem>();
+            Quests = new List<PlayerQuest>();
         }
     }
 }
